c867453883198c718c64b306819eccb2698bde5e
Zaim Ramlan <zaem.muz@gmail.com>
2019-02-08 19:16:20 +0800

    initial project creation

---
+++ ProgressBarKit/ProgressBarKit.h
@@ -0,0 +1,19 @@
+//
+//  ProgressBarKit.h
+//  ProgressBarKit
+//
+//  Created by Zaim Ramlan on 08/02/2019.
+//  Copyright © 2019 Zaim Ramlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProgressBarKit.
+FOUNDATION_EXPORT double ProgressBarKitVersionNumber;
+
+//! Project version string for ProgressBarKit.
+FOUNDATION_EXPORT const unsigned char ProgressBarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressBarKit/PublicHeader.h>
+
+
